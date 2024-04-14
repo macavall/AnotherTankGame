d294b9f14dc9fc3a9c453e8b8b89214d9f1982e7
Matthew Cavallo <macavall@microsoft.com>
2024-04-14 16:49:04 -0500

    test

--- Program.cs
+++ Program.cs
@@ -1,66 +1,112 @@
 ﻿using Raylib_cs;
 using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 
 namespace TankGame
 {
+    public class Projectile()
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public float projectileSpeed = 200.0f;
+    }
+
     class Program
     {
+        // Define the window dimensions
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+
+        // Define the player (square) properties
+        static Rectangle player = new Rectangle(400, 225, 30, 30);
+        static float playerSpeed = 150.0f;
+        static Vector2 lastDirection = new Vector2(0, -1);
+
+        // List to keep track of projectiles
+        static List<Projectile> projectiles = new List<Projectile>();
+        //static float projectileSpeed = 200.0f;
+
         static void Main(string[] args)
         {
-            const int screenWidth = 800;
-            const int screenHeight = 450;
-            const int spriteSpeed = 5;
+            // Initialize the window
+            Raylib.InitWindow(screenWidth, screenHeight, "Raylib Example - Square and Projectiles");
+            Raylib.SetTargetFPS(60);
 
-            Raylib_cs.Raylib.InitWindow(screenWidth, screenHeight, "Sprite Movement Example");
-
-            // Load sprite image
-            Texture2D spriteUp = Raylib_cs.Raylib.LoadTexture("C:\\Users\\macavall\\Pics\\TankUp.png");
-            Texture2D spriteRight = Raylib_cs.Raylib.LoadTexture("C:\\Users\\macavall\\Pics\\TankRight.png");
-            Texture2D spriteDown = Raylib_cs.Raylib.LoadTexture("C:\\Users\\macavall\\Pics\\TankDown.png");
-            Texture2D spriteLeft = Raylib_cs.Raylib.LoadTexture("C:\\Users\\macavall\\Pics\\TankLeft.png");
-
-            Texture2D sprite = spriteUp;
-
-            // Set sprite position
-            Vector2 position = new Vector2(screenWidth / 2 - spriteUp.Width / 2, screenHeight / 2 - spriteUp.Height / 2);
-
-            Raylib_cs.Raylib.SetTargetFPS(60);
-
-            while (!Raylib_cs.Raylib.WindowShouldClose())
+            while (!Raylib.WindowShouldClose())
             {
-                // Move sprite based on arrow keys
-                if (Raylib_cs.Raylib.IsKeyDown(KeyboardKey.Right))
-                {
-                    position.X += spriteSpeed;
-                    sprite = spriteRight;
-                }
-                if (Raylib_cs.Raylib.IsKeyDown(KeyboardKey.Left))
-                {
-                    position.X -= spriteSpeed;
-                    sprite = spriteLeft;
-                }
-                if (Raylib_cs.Raylib.IsKeyDown(KeyboardKey.Down))
-                {
-                    position.Y += spriteSpeed;
-                    sprite = spriteDown;
-                }
-                if (Raylib_cs.Raylib.IsKeyDown(KeyboardKey.Up))
-                {
-                    position.Y -= spriteSpeed;
-                    sprite = spriteUp;
-                }
+                // Update
+                UpdateGame();
 
                 // Draw
-                Raylib_cs.Raylib.BeginDrawing();
-                Raylib_cs.Raylib.ClearBackground(Color.White);
-                Raylib_cs.Raylib.DrawTexture(sprite, (int)position.X, (int)position.Y, Color.White);
-                Raylib_cs.Raylib.EndDrawing();
+                DrawGame();
             }
 
-            Raylib_cs.Raylib.UnloadTexture(sprite);
-            Raylib_cs.Raylib.CloseWindow();
+            // Close window and unload resources
+            Raylib.CloseWindow();
+        }
+
+        static void UpdateGame()
+        {
+            // Movement logic for the player
+            if (Raylib.IsKeyDown(KeyboardKey.Right))
+            {
+                player.X += playerSpeed * Raylib.GetFrameTime();
+                lastDirection = new Vector2(1, 0);
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.Left))
+            {
+                player.X -= playerSpeed * Raylib.GetFrameTime();
+                lastDirection = new Vector2(-1, 0);
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.Up))
+            {
+                player.Y -= playerSpeed * Raylib.GetFrameTime();
+                lastDirection = new Vector2(0, -1);
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.Down))
+            {
+                player.Y += playerSpeed * Raylib.GetFrameTime();
+                lastDirection = new Vector2(0, 1);
+            }
+
+            // Shooting projectiles
+            if (Raylib.IsKeyPressed(KeyboardKey.Space))
+            {
+                projectiles.Add(new Rectangle(player.X + player.Width / 2, player.Y + player.Height / 2, 10, 20));
+            }
+
+            // Update projectiles positions
+            for (int i = projectiles.Count - 1; i >= 0; i--)
+            {
+                projectiles[i].X += lastDirection.X * projectileSpeed * Raylib.GetFrameTime();
+                projectiles[i].Y += lastDirection.Y * projectileSpeed * Raylib.GetFrameTime();
+
+                // Remove projectiles when they go off screen
+                if (projectiles[i].X > screenWidth || projectiles[i].x < 0 ||
+                    projectiles[i].y > screenHeight || projectiles[i].y < 0)
+                {
+                    projectiles.RemoveAt(i);
+                }
+            }
+        }
+
+        static void DrawGame()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            // Draw player
+            Raylib.DrawRectangleRec(player, Color.BLUE);
+
+            // Draw projectiles
+            foreach (var projectile in projectiles)
+            {
+                Raylib.DrawRectangleRec(projectile, Color.RED);
+            }
+
+            Raylib.EndDrawing();
         }
     }
 }
